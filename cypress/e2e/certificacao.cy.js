062e7187bbecf66719e1d4bdebf902a26c219a71
thiagomve <thiagonfk@gmail.com>
2026-03-04 15:54:08 -0300

    Adicionando describe para botões de redes sociais

--- cypress/e2e/certificacao.cy.js
+++ cypress/e2e/certificacao.cy.js
@@ -2,12 +2,9 @@ beforeEach(() => {
   cy.visit('https://qualidade.apprbs.com.br/certificacao/')
 })
 
+
+// Validação dos botões "Quero me certificar
 describe('Validação dos botões "Quero me certificar"', () => {
-
-  // beforeEach(() => {
-  //   cy.visit('https://qualidade.apprbs.com.br/certificacao/')
-  // })
-
   it('Deve validar existência e redirecionamento dos 3 botões', () => {
 
     const botoes = ['#ivw5ng', '#i72bga', '#ixy3u1']
@@ -28,9 +25,10 @@ describe('Validação dos botões "Quero me certificar"', () => {
 
 })
 
+
+// Validação do botão "Saiba mais" do Header
 describe('Validação do botão "Saiba mais" do Header', () => {
-  it('Deve validar a existência e redirecionamento', () => {
-    // cy.visit('https://qualidade.apprbs.com.br/certificacao/')
+  it('Deve validar a existência e redirecionamento do botão', () => {
     cy.get('#i7r4lj')
       .should('exist')
       .should('be.visible')
@@ -40,6 +38,36 @@ describe('Validação do botão "Saiba mais" do Header', () => {
         cy.log(`O botão "Saiba mais" redireciona para: ${href}`)
       })
   })
+})
+
+
+// Validação dos botões/ícones de redes sociais
+describe('Validação dos botões/ícones de redes sociais', () => {
+
+  const botoes = [
+    { id: '#io0o4o', nome: 'facebook', redirect: 'https://www.facebook.com/CanalRubeus' },
+    { id: '#il7i3x', nome: 'instagram', redirect: 'https://www.instagram.com/canalrubeus/' },
+    { id: '#i2m2tn', nome: 'linkedin', redirect: 'https://www.linkedin.com/company/rubeus/' },
+    { id: '#ifpwp7', nome: 'youtube', redirect: 'https://www.youtube.com/rubeus' }
+  ]
+
+  botoes.forEach(({ id, nome, redirect }) => {
+    it(`Deve validar existência e redirecionamento do ícone/botão ${nome}`, () => {
+      cy.get(id)
+        .should('exist')
+        .should('be.visible')
+        .should('not.be.disabled')
+        .should('have.attr', 'href')
+        .then((href) => {
+          cy.log(`O botão ${nome} está redirecionando para: ${href}`)
+
+          if (!href.includes(redirect)) {
+            throw new Error(`Redirecionamento incorreto. Esperado: ${redirect} | Atual: ${href}`)
+          }
+        })
+    })
+  })
+
 })
 
 
